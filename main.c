87987810dd283fdb5ef0a1c09ace94a30cf7446e
AyobamiAdebesin <froshofficial@gmail.com>
2022-04-19 13:23:49 +0100

    Modified _print.c

--- main.c
+++ main.c
@@ -9,17 +9,30 @@
  */
 int main(void)
 {
-    int len1, len2, len3, len4;
-    len1 = _printf("Let's try to printf a simple sentence.\n");
-    printf("Len:[%d]\n", len1);
+    int len1a, len1b, len2a, len2b;
+    int len3a, len3b, len4a, len4b;
+    len1a = _printf("Let's try to printf a simple sentence.\n");
+    len1b = printf("Let's try to printf a simple sentence.\n");
+    printf("Len1a:[%d]\n", len1a);
+    printf("Len1b:[%d]\n", len1b);
 
-    len2 = _printf("Character:[%c]\n", 'H');
-    printf("Len:[%d]\n", len2);
+    
+    len2a = _printf("Character:[%c]\n", 'H');
+    len2b = printf("Character:[%c]\n", 'H');
+    printf("Len2a:[%d]\n", len2a);
+    printf("Len2b:[%d]\n", len2b);
 
-    len3 = _printf("String:[%s]\n", "I am a string !");
-    printf("Len:[%d]\n", len3);
 
-    len4 = _printf("Percent:[%%]\n");
-    printf("Len:[%d]\n", len4);
+    len3a = _printf("String:[%s]\n", "I am a string !");
+    len3b = printf("String:[%s]\n", "I am a string !");
+    printf("Len3a:[%d]\n", len3a);
+    printf("Len3b:[%d]\n", len3b);
+
+    len4a = _printf("Percent:[%%]\n");
+    len4b = printf("Percent:[%%]\n");
+    _printf("Len4a:[%d]\n", len4a);
+    printf("Len4b:[%d]\n", len4b);
+
+
     return (0);
 }
